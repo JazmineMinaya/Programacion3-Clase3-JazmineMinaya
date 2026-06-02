48cacbe3149b16ef00bd7ad3a33a8d1c1817b8b3
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-01 20:12:11 -0400

    Declaración de los atributos, constructores, getters y setters de la clase Estudiante

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -1,3 +1,37 @@
 public class Estudiante {
-    
+    private String nombre;
+    private int edad;
+    private double indiceAcademico;
+
+    public Estudiante() {
+        setNombre("");
+        setEdad(0);
+        setIndiceAcademico(0);
+    }
+
+    public Estudiante(String nombre, int edad, double indiceAcademico) {
+        setNombre(nombre);
+        setEdad(edad);
+        setIndiceAcademico(indiceAcademico);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public double getIndiceAcademico() {
+        return indiceAcademico;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setIndiceAcademico(double indiceAcademico) {
+        this.indiceAcademico = indiceAcademico;
+    }
 }
