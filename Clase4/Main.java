1a9a4f81b863f7e42c887ec6d76db1933b91dade
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-08 19:57:59 -0400

    Creación del método mostrar menú

--- Clase4/Main.java
+++ Clase4/Main.java
@@ -2,6 +2,15 @@ package Clase4;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("\n===========================");
+        System.out.println("    ARCHIVAR ESTUDIANTE");
+        System.out.println("===========================");
+        System.out.println("\n1. Agregar estudiante");
+        System.out.println("2. Mostrar estudiantes");
+        System.out.println("3. Salir");
     }
 }
