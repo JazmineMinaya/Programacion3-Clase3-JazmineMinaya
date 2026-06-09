fc3649d8c759fb8db9cab89b3e1942c31c83d97e
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-08 20:40:40 -0400

    Creación de la clase Estudiante, sus atributos, constructores, getters y setters y la creación del método agregarEstudiante

--- Clase4/Estudiante.java
+++ Clase4/Estudiante.java
@@ -1,5 +1,39 @@
 package Clase4;
 
 public class Estudiante {
-    
+    private String nombre;
+    private int edad;
+    private String matricula;
+
+    public Estudiante() {
+        setNombre("");
+        setEdad(0);
+        setMatricula("");
+    }
+
+    public Estudiante(String nombre, int edad, String matricula) {
+        setNombre(nombre);
+        setEdad(edad);
+        setMatricula(matricula);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
--- Clase4/Main.java
+++ Clase4/Main.java
@@ -1,8 +1,13 @@
 package Clase4;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Main {
-    public static void main(String[] args) {
+    static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+       
     }
 
     public static void mostrarMenu() {
@@ -12,5 +17,36 @@ public class Main {
         System.out.println("\n1. Agregar estudiante");
         System.out.println("2. Mostrar estudiantes");
         System.out.println("3. Salir");
+    }
+
+    public static void agregarEstudiante() {
+        System.out.println("AGREGAR ESTUDIANTE:\n");
+
+        System.out.print("Ingrese el nombre:");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingrese la edad: ");
+        int edad = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Ingrese la matricula: ");
+        String matricula = scanner.nextLine();
+
+        Estudiante estudiante = new Estudiante(nombre, edad, matricula);
+
+        try {
+            FileWriter archivoEstudiante = new FileWriter("archivoEstudiante.txt", true);
+
+            archivoEstudiante.write("Nombre: " + estudiante.getNombre() + "\n");
+            archivoEstudiante.write("Edad: " + estudiante.getEdad() + "\n");
+            archivoEstudiante.write("Matricula: " + estudiante.getMatricula() + "\n");
+
+            archivoEstudiante.close();
+
+            System.out.println("\nEl estudiante ha sido agregado exitosamente al archivo.");
+        }
+        catch(Exception e) {
+            System.out.println("\nError: " + e.getMessage());
+        }
     }
 }
