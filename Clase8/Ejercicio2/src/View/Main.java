79665df8da0b208f9e33985939561a18516d98ff
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-06-29 23:14:25 -0400

    Declaración del main, creación del método main y start

--- Clase8/Ejercicio2/src/View/Main.java
+++ Clase8/Ejercicio2/src/View/Main.java
@@ -1,5 +1,24 @@
 package View;
 
-public class Main {
-    
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/Principal.fxml"));
+
+        Scene scene = new Scene(loader.load(), 400, 400);
+
+        stage.setScene(scene);
+        stage.setTitle("Conversor de Monedas");
+        stage.show();
+    }
 }
